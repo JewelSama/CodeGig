68815bd28db7f57aa4b1d5abec078910cbad015f
Jewel Osafile <osafilejewel@gmail.com>
2022-08-02 15:42:40 +0100

    gigs router

--- routes/gigs.routes.js
+++ routes/gigs.routes.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const db = require('../config/db')
+const Gig = require('../models/Gig')
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Gigs'))
+router.get('/', (req, res) => 
+    Gig.findAll()
+      .then(gigs => {
+        console.log(gigs)
+        res.sendStatus(200)
+      })
+      .catch(err => console.log(err))
+)
 
 module.exports = router
